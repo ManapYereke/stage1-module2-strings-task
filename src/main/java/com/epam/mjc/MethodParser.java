f68f2f6bd50ae1f4ea9af6fc2bc54dfa3717ac17
ManapYereke <manap.ereke@gmail.com>
2023-06-19 16:07:54 +0600

    wedg

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,8 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
@@ -20,6 +23,51 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    String[] parts = signatureString.split("\\s+"); // Split the signature string by whitespace
+
+    int startIndex = 0;
+    String accessModifier = parts[0]; // First part is the access modifier
+    if (isAccessModifier(accessModifier)) {
+        startIndex = 1;
+    } else {
+        accessModifier = null;
     }
+
+    String returnType = parts[startIndex]; // Next part is the return type
+
+    int endIndex = signatureString.indexOf('(');
+    String methodName = parts[startIndex + 1];
+    int index = methodName.indexOf("(");
+    methodName = methodName.substring(0, index);
+    String argumentsString = signatureString.substring(endIndex + 1, signatureString.indexOf(')')); // Extract the argument string
+    List<MethodSignature.Argument> arguments = parseArguments(argumentsString); // Parse the arguments
+
+    MethodSignature methodSignature = new MethodSignature(methodName, arguments);
+    methodSignature.setAccessModifier(accessModifier);
+    methodSignature.setReturnType(returnType);
+    return methodSignature;
+}
+
+private List<MethodSignature.Argument> parseArguments(String argumentsString) {
+    List<MethodSignature.Argument> arguments = new ArrayList<>();
+    if (!argumentsString.isEmpty()) {
+        String[] argumentParts = argumentsString.split(",");
+        for (String argumentPart : argumentParts) {
+            String[] argumentComponents = argumentPart.trim().split("\\s+");
+            String argumentType = argumentComponents[0];
+            String argumentName = argumentComponents[1];
+            MethodSignature.Argument argument = new MethodSignature.Argument(argumentType, argumentName);
+            arguments.add(argument);
+        }
+    }
+    return arguments;
+}
+
+private boolean isAccessModifier(String accessModifier) {
+    return accessModifier.equals("public")
+            || accessModifier.equals("protected")
+            || accessModifier.equals("private")
+            || accessModifier.equals("default");
+}
+
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -2,17 +2,30 @@ package com.epam.mjc;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
 
 public class StringSplitter {
 
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimeters to it. Keeps order of result
+     * substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
+
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> result = new ArrayList<>();
+        StringBuilder regex = new StringBuilder();
+        regex.append("[");
+        delimiters.stream().forEach(delimiter -> regex.append(delimiter + "/"));
+        regex.append("]");
+        String[] str = source.split(regex.toString());
+        for(String s : str){
+            if(s.length() >= 1)
+                result.add(s);
+        }
+        return result;
     }
 }
